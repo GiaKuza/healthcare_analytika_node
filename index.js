ce4e327a5dda93bc45dc13fb5bfc749754c20e6e
Giancarlo Coca <giancarlococa@Giancarlos-MacBook-Pro.local>
2021-08-03 17:44:15 -0700

    installed basic npm packages and successfully connected to mongodb

---
+++ index.js
@@ -0,0 +1,19 @@
+const connectDB = require('./startup/db');
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const collections = require('./routes/collections');
+const auth = require('./routes/auth');
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/collections', collections);
+app.use('/api/auth', auth);
+
+app.listen(5000, function () {
+    console.log("Server started. Listening on port 5000.");
+    connectDB();
+})
---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
